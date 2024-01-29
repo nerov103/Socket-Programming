917a0ae98bf572df1da348b7c047ce54f163b486
nerov103 <nibir01000@gmail.com>
2024-01-29 22:30:06 +0600

    new file added in this file connected to a same network

---
+++ one.py
@@ -0,0 +1,21 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("192.168.0.102", 8080))
+s.listen()
+clientObjcet, desl = s.accept()
+print("connedact to> ", desl)
+
+while True:
+    msg = clientObjcet.recv(1024)
+    print(msg.decode())
+
+    #brack
+    if len(msg)==0:
+        s.close()
+        break
+
+    
+
+
+
